dd41826a749669ab55f9e4a3e1c6c35e8cef4a92
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-05-11 10:39:25 +0700

    Authorize API Request

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+    has_secure_password
     #memberikan validasi agar nama email dan password tidak boleh kosong
     validates_presence_of :name, :email, :password_digest
 end

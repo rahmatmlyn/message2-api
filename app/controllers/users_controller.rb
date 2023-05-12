ac216069c3b76278edb7f084479f401b430759ef
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-05-12 20:17:57 +0700

    add routes viewuser and catatan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
 
+  # routes viewuser
   def index
     users = User.where.not(id: @current_user.id)
     json_response(users)
   end
 
+  # membuat user dengan memasukkan email serta password
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    # 
+    has_many :conversations, foreign_key: 'sender_id'
+    has_many :conversations, foreign_key: 'receiver_id'
+    has_many :messages
     #memberikan validasi agar nama email dan password tidak boleh kosong
     validates_presence_of :name, :email, :password_digest
 end
